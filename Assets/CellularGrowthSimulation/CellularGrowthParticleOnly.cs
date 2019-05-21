c0df2cadb22f9372d85ca5f16bf57d2153133522
祐貴 林 <y.hayashi@y-hayashi-10.Q-DOMAIN>
2019-05-21 14:02:38 +0900

    cgs

---
+++ Assets/CellularGrowthSimulation/CellularGrowthParticleOnly.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Seiro.GPUSandbox.CellularGrowthSimulation
+{
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public struct CellularParticle
+	{
+		public Vector2 position;	// 粒子の座標
+		public Vector2 velocity;	// 粒子の保持している速度
+		public float rdius;			// 粒子の半径
+		public float threshold;		// 粒子の最大半径
+		public int links;			// 接続しているエッジの数
+		public uint alive;			// 活性化フラグ
+	}
+
+	public class CellularGrowthParticleOnly : MonoBehaviour
+	{
+
+	}
+}
--- Assets/Common/Util.cs
+++ Assets/Common/Util.cs
@@ -1,8 +1,34 @@
 ﻿using UnityEngine;
 using System;
+using System.Runtime.InteropServices;
 
 namespace Seiro.GPUSandbox
 {
+
+	public sealed class PingPongBuffer : IDisposable
+	{
+		ComputeBuffer[] _buffers;
+		int _read = 0, _write = 1;
+
+		public PingPongBuffer(int count, Type type)
+		{
+			_buffers = new ComputeBuffer[2];
+			_buffers[0] = new ComputeBuffer(count, Marshal.SizeOf(type), ComputeBufferType.Default);
+			_buffers[1] = new ComputeBuffer(count, Marshal.SizeOf(type), ComputeBufferType.Default);
+		}
+
+		public ComputeBuffer read { get { return _buffers[_read]; } }
+		public ComputeBuffer write { get { return _buffers[_write]; } }
+
+		public void Dispose()
+		{
+			UtilFunc.ReleaseBuffer(ref _buffers[0]);
+			UtilFunc.ReleaseBuffer(ref _buffers[1]);
+			_buffers = null;
+		}
+
+		public void Swap() { var tmp = _read; _read = _write; _write = tmp; }
+	}
 
     public static class UtilFunc
     {
