852f5de6aece068b7fdf2903ad27cd7c224d5f6e
祐貴 林 <y.hayashi@y-hayashi-10.Q-DOMAIN>
2019-06-25 21:14:42 +0900

    adjust fluids solver

---
+++ Assets/Common/Follower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Seiro.GPUSandbox
+{
+	public sealed class Follower : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform _target;
+		[SerializeField]
+		private bool _followingPosition = true;
+		[SerializeField]
+		private Vector3 _offset;
+		[SerializeField]
+		private bool _followingAngles = false;
+
+		private void Update()
+		{
+			if (!_target) return;
+			if (_followingPosition)
+			{
+				Vector3 pos = _target.transform.position + _offset;
+				transform.position = pos;
+			}
+			if (_followingAngles)
+			{
+				Quaternion rotation = _target.transform.rotation;
+				transform.rotation = rotation;
+			}
+		}
+	}
+}
