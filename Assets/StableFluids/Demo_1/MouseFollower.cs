4af952601ef0957879ca2366f8b337d50f329f67
祐貴 林 <y.hayashi@y-hayashi-10.Q-DOMAIN>
2019-08-27 20:28:56 +0900

    mouse follower demo

---
+++ Assets/StableFluids/Demo_1/MouseFollower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Seiro.GPUSandbox.StableFluids
+{
+	public sealed class MouseFollower : MonoBehaviour
+	{
+		public Rigidbody2D body;
+
+		private void FixedUpdate()
+		{
+			if (body && Input.GetMouseButton(0))
+			{
+				Vector3 mPos = Input.mousePosition;
+				mPos.z = -Camera.main.transform.position.z;
+				Vector3 wPos = Camera.main.ScreenToWorldPoint(mPos);
+				Vector3 diff = wPos - transform.position;
+				body.AddForce(diff);
+			}
+		}
+	}
+}
